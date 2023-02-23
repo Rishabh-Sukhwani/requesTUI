877d69710b99b6007ebcb4a43ba69ad3cd5ebf54
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2023-02-23 21:32:57 +0530

    refactor: create UI directory to layout widgets

---
+++ ui/layout.go
@@ -0,0 +1,39 @@
+package ui
+
+import (
+	"github.com/rivo/tview"
+	"example.com/gotui/handlers"
+)
+
+func RunUI() {
+	app := tview.NewApplication()
+	
+	form := tview.NewForm().
+			AddInputField("URL", "", 500, nil, nil).
+			AddDropDown("Method", []string{"GET", "POST", "PATCH", "DELETE"}, 0, nil).
+			AddTextArea("Headers", "", 0, 7, 0, nil).
+			AddTextArea("Body", "", 0, 10, 0, nil)
+
+	bufferBox := tview.NewTextView()
+	
+	textView := tview.NewTextView().
+				SetDynamicColors(true).
+				SetRegions(true).
+				SetWordWrap(true)
+
+	form.AddButton("Submit", func() {
+		handlers.PrintInputValues(form, textView)
+	})
+	
+	flex := tview.NewFlex().
+			AddItem(form, 0, 4, true).
+			AddItem(bufferBox, 0, 1, false).
+			AddItem(textView, 0, 4, false)
+
+
+	form.SetBorder(true).SetTitle("POSTMAN").SetTitleAlign(tview.AlignCenter)
+	textView.SetTitle("RESPONSE:").SetTitleAlign(tview.AlignCenter)
+	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
