ec05f2e1e841ccd039c0fd66cc3a8dca3ac4c212
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2023-02-23 18:45:41 +0530

    feat: Add POST request functionality

--- handlers/handle_submit.go
+++ handlers/handle_submit.go
@@ -3,13 +3,14 @@ package handlers
 import (
 	"github.com/rivo/tview"
 	"example.com/gotui/services"
-	
 )
 
 func PrintInputValues(form *tview.Form, textView *tview.TextView) {
 	url := form.GetFormItemByLabel("URL").(*tview.InputField).GetText()
 	headers := form.GetFormItemByLabel("Headers").(*tview.TextArea).GetText()
+	body := form.GetFormItemByLabel("Body").(*tview.TextArea).GetText()
 
-	textView.SetText(headers)
-	services.GetRequest(url, headers, textView)
+	//textView.SetText(headers)
+	//services.GetRequest(url, headers, textView)
+	services.PostRequest(url, headers, body, textView)
 }
---
+++ services/post.go
@@ -0,0 +1,68 @@
+package services
+
+import (
+	"net/http"
+	"github.com/rivo/tview"
+	"fmt"
+	"bytes"
+	"encoding/json"
+)
+
+func PostRequest(url string, headers string, body string, textView *tview.TextView) {
+	
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(body)))
+
+	if err != nil {
+		textView.SetText(fmt.Sprintf("%v", err))
+		return
+	} 
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	
+	if headers != "" {
+		var headerMap map[string]string
+		err = json.Unmarshal([]byte(headers), &headerMap)
+		if err != nil {
+			textView.SetText(fmt.Sprintf("%v", err))
+			return
+		}
+
+		for key, value := range headerMap {
+			req.Header.Set(key, value)
+		}
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		textView.SetText(fmt.Sprintf("%v", err))
+		return
+	}
+	defer resp.Body.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		textView.SetText(fmt.Sprintf("%v", err))
+		return
+	}
+
+	var jsonData interface{}
+	err = json.Unmarshal(buf.Bytes(), &jsonData)
+	if err != nil {
+		textView.SetText(fmt.Sprintf("%v", err))
+		return
+	}
+
+	prettyJson, err := json.MarshalIndent(jsonData, "", "	")
+	if err != nil {
+		textView.SetText(fmt.Sprintf("%v", err))
+		return
+	}
+
+
+
+	textView.SetText(string(prettyJson))
+	
+
+}
